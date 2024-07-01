de83e99c7b7bd90c0afae0e0a80e0511a9bb4dab
chessplayer123 <ieriemkin20022002@gmail.com>
2024-07-01 13:57:28 +0300

    Point coordinates now draw on canvas

--- web/rmilib_par.cpp
+++ web/rmilib_par.cpp
@@ -1,29 +1,7 @@
-#include <emscripten.h>
-#include <emscripten/bind.h>
-#include <vector>
-#include <sstream>
-
 #include "rmilib_shared.hpp"
-#include "rmilib/rmi_parallel.hpp"
-
-
-template<int N>
-std::vector<rmi::Vector3f> tree_par_intersects_ray(
-    const rmi::Tree<WebGLMesh, N>& tree,
-    const rmi::Ray<float>& ray,
-    int threads_count
-) {
-    return rmi::parallel::pool_intersects(ray, tree, threads_count);
-}
 
 
 EMSCRIPTEN_BINDINGS(module) {
-    register_shared();
-
-    register_tree<1>("KDTree")
-        .function("par_intersects", &tree_par_intersects_ray<1>);
-    register_tree<2>("Quadtree")
-        .function("par_intersects", &tree_par_intersects_ray<2>);
-    register_tree<3>("Octree")
-        .function("par_intersects", &tree_par_intersects_ray<3>);
+    register_shared()
+        .function("poolIntersectsTree", &rmi::Ray<float>::pool_intersects<WebGLMesh>);
 }
